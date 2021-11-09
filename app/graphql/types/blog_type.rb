47c69af15e9837ff5f7f1d10275687142001f6ab
theodoros-git <kossitheodore@yahoo.com>
2021-11-09 14:19:20 +0100

    rails graphql

---
+++ app/graphql/types/blog_type.rb
@@ -0,0 +1,9 @@
+module Types
+  class BlogType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: true
+    field :content, String, null: true
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
